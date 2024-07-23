9dce257f4c6fba91de8b084a74208bbef864276c
Welcome ! <usmanubaid9@gmail.com>
2024-07-23 13:10:58 +0200

    parsing incoming events

--- query/index.js
+++ query/index.js
@@ -4,8 +4,30 @@ const app = express();
 
 app.use(express.json());
 
-app.post("/events", (req, res) => {});
-app.get("/events", (req, res) => {});
+const posts = {};
+
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "PostCreated") {
+    const { id, title } = data;
+
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === "CommentCreated") {
+    const { commentId, content, postId } = data;
+    const post = posts[postId];
+
+    post.comments.push({ id: commentId, content });
+  }
+  console.log(posts);
+  res.send({});
+});
+
+app.get("/events", (req, res) => {
+  res.send(posts);
+});
 
 app.listen(4002, () => {
   console.log("Listening on PORT: 4002");
