40cec47bd7dd5b8c55a31291fb0b532c4bf64196
Welcome ! <usmanubaid9@gmail.com>
2024-09-10 13:16:47 +0200

    feat: posts service creation

---
+++ posts/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+
+const app = express();
+const port = 4000;
+
+app.use(express.json());
+
+const posts = {};
+
+app.post("/posts", (req, res) => {
+  const { title } = req.body;
+  const id = Math.floor(Math.random() * 1000);
+  posts[id] = { id, title };
+
+  return res.status(201).send(posts[id]);
+});
+
+app.get("/posts", (req, res) => {
+  return res.send(posts);
+});
+
+app.listen(port, () => {
+  console.log(`Listening on PORT: ${port}`);
+});
