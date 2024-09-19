9410f853d2bb946d61a95408befa12f1ce0d1bd1
Welcome ! <usmanubaid9@gmail.com>
2024-09-19 18:28:33 +0200

    feat: using the query service

--- client/src/components/CommentList.jsx
+++ client/src/components/CommentList.jsx
@@ -1,24 +1,4 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
-
-const CommentList = ({ postId }) => {
-  const [comments, setComments] = useState([]);
-
-  const getComments = async () => {
-    try {
-      const res = await axios.get(
-        `http://localhost:4001/posts/${postId}/comments`
-      );
-      setComments(res.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    getComments();
-  }, []);
-
+const CommentList = ({ comments }) => {
   const renderComments = comments.map((comment) => {
     return <li key={comment.commentId}>{comment.content}</li>;
   });
